924f93802ef5b23eae5869f9570bae53d96a5c34
Alix Leon <alixlm19@hotmail.com>
2019-06-12 14:01:17 -0400

    Update SHA_256.java

--- src/sha_256/SHA_256.java
+++ src/sha_256/SHA_256.java
@@ -3,7 +3,7 @@ package sha_256;
 import java.math.BigInteger;
 
 public class SHA_256 {
-	private static int[] words = {0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+	private static int[] K = {0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
 							  0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
 							  0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
 							  0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
@@ -15,20 +15,52 @@ public class SHA_256 {
 	
 	public BigInteger digest(String message) {
 		String bin = strToBin(message);
+		
 		//Checks if the binary string size is a multiple of 512
-		if(bin.length() % 512 != 0) {
-			bin = bin + '1';
-			int k = 0;
-			while((bin.length() + 1 + k) % 512 != 448 % 512) {
-				bin = bin + '0';
-				k++;
-			}
+		System.out.println("bin: " + bin);
+		int l = bin.length();
+		bin += '1';
+		int k = 0;
+		
+		//Appends K bits to the binString until l + 1 + k is congruent to 448 mod 512
+		while((l + 1 + k) % 512 != 448 % 512) {
+			bin += "0";
+			k++;
 		}
 		
-		System.out.println(bin);
+		//Represent the length l < 2^64 as a 64 bit number
+		String lBinString = Long.toBinaryString(l);
+		int lBinStringLength = lBinString.length();
+		for(int i = 0; i < 64 - lBinStringLength; i++) {
+			lBinString = "0" + lBinString;
+		}
+		bin += lBinString;					//We end up with a 512 bit binString
+		
+		//Decompose to binString
+		decompose(bin);
+		
 		BigInteger msg = new BigInteger("1");
 
 		return msg;
+	}
+	
+	private String decompose(String M) {
+		String[] W = new String[64];
+		System.out.println(M.length());
+		
+		//Gets the first 16 blocks by splitting M in 32-bit b
+		for(int i = 0; i < 16; i++) {
+			W[i] = M.substring(i * 32, (i + 1) * 32);
+			//System.out.println(W[i] + " " +  W[i].length());
+		}
+		
+		//the remaining 48 are obtained with the formula:
+		for(int i = 16; i < 64; i++) {
+			String a = sigma_1(W[i - 2]);
+			//System.out.println(W[i].length());
+		}
+		
+		return "";
 	}
 	
 	private String strToBin(String message) {
@@ -45,39 +77,71 @@ public class SHA_256 {
 		return binary;
 	}
 	
-	public int RotR(int A, int n) {
+	public String RotR(String A, int n) {
 		/**Circular right shift of n bits of the binary word A**/
-		int lsb;
 		for(int i = 0; i < n; i++) {
-			lsb = (int) A & 0b1;			//Returns the LSB of A
-			A >>= 1; 						//Shift A 1 bit to the right
-			A -= 0x80000000;				//Removes the MSB result of the bit shifting operation
-			A |= lsb << 31;					//Replaces the MSB with the LSB
+			A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);
 		}
 		return A;
 	}
 	
-	public int Ch(int X, int Y, int Z) {
-		return (X & Y) ^ (~X & Z);
+	private String AND(String A, String B) {
+		String result = "";
+		for(int i = 0; i < A.length(); i++) {
+			result += (A.charAt(i) == '1' && B.charAt(i) == '1') ? "1" : "0";
+		}
+		return result;
 	}
 	
-	public int Maj(int X, int Y, int Z) {
-		return (X & Y) ^ (X & Z) ^ (Y & Z);
+	private String XOR(String ...args) {
+		String result = args[0];
+		if(args.length > 1) {
+			String temp;
+			for(int i = 1; i < args.length; i++) {
+				temp = "";
+				for(int j = 0; j < args[i].length(); j++) {
+					temp += (result.charAt(j) == args[i].charAt(j))? "0" : "1";
+				}
+				result = temp;
+			}
+		}
+		//System.out.println("asdf" + result.length());
+		return result;
 	}
 	
-	public int sum_0(int X) {
-		return RotR(X, 2) ^ RotR(X, 13) ^ RotR(X, 22);
+	private String OR() {
+		return "";
 	}
 	
-	public int sum_1(int X) {
-		return RotR(X, 6) ^ RotR(X, 11) ^ RotR(X, 25);
+	public String complement(String A) {
+		String result = "";
+		for(char c : A.toCharArray()) {
+			result += c == '1' ?  "0" : "1";
+		}
+		return result;
 	}
 	
-	public int sigma_0(int X) {
-		return RotR(X, 7) ^ RotR(X, 18) ^ RotR(X, 3);
+	public String Ch(String X, String Y, String Z) {
+		return XOR(AND(X, Y), AND(complement(X), Z));
+	}
+	
+	public String Maj(String X, String Y, String Z) {
+		return XOR(AND(X, Y), AND(X, Z), AND(Y, Z));
+	}
+	
+	public String sum_0(String X) {
+		return XOR(RotR(X, 2), RotR(X, 13), RotR(X, 22));
+	}
+	
+	public String sum_1(String X) {
+		return XOR(RotR(X, 6), RotR(X, 11), RotR(X, 25));
+	}
+	
+	public String sigma_0(String X) {
+		return XOR(RotR(X, 7), RotR(X, 18), RotR(X, 3));
 	}
 
-	public int sigma_1(int X) {
-		return RotR(X, 17) ^ RotR(X, 19) ^ RotR(X, 10);
+	public String sigma_1(String X) {
+		return XOR(RotR(X, 17), RotR(X, 19), RotR(X, 10));
 	}
 }
